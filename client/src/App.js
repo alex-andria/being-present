3713a02b017bcbedd16002b500061fafb30da246
bbland1 <104288486+bbland1@users.noreply.github.com>
2022-11-17 19:37:12 -0500

    added journal entry modal and skeleton of list

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,80 @@
+import React from "react";
 import { useState, useEffect } from "react";
+import AddJournal from "./Components/AddJournal/AddJournal";
+import "./App.css"
+// import Journal from "./Components/Journal";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    fetch("/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
-  }, []);
+  // useEffect(() => {
+  //   fetch("/hello")
+  //     .then((r) => r.json())
+  //     .then((data) => setCount(data.count));
+  // }, []);
+  const[journals, setJournals] = useState([]);
+  const [show, setShow] = useState(false)
+
+  function submitJournal(e) {
+    // this would need the info for pushing the journal to the db
+    console.log("journal added");
+    e.preventDefault();
+    setShow(false);
+  }
+
 
   return (
-    <div className="App">
-      <h1>Page Count: {count}</h1>
-    </div>
+    // <div className="App">
+    //   <h1>Page Count: {count}</h1>
+    // </div>
+    <>
+      <h1>Welcome</h1>
+
+      {/* Logo/relaxing css */}
+      <br></br>
+
+      <h2>Be present. Add your entry.</h2>
+      {/* Create New Journal */}
+      <br></br>
+
+      <h2>MODAL - journal entry form:</h2>
+      <button onClick={() => setShow(true)} className="open-modal">Add Journal</button>
+      <AddJournal onClose={() => setShow(false)} onSubmit={submitJournal} show={show}/>
+      {/* Form */}
+      <br></br>
+
+      {/* Journal List: */}
+      <h2>Journal List Below:</h2>
+      <div className="journal-list">
+        <div className="journal-entry">
+          <div className="journal-image"><p>11/17/22</p></div>
+          <p>Generated Suggestion</p>
+        </div>
+        <div className="journal-entry">
+          <div className="journal-image"><p>11/17/22</p></div>
+          <p>Generated Suggestion</p>
+        </div>
+        <div className="journal-entry">
+          <div className="journal-image"><p>11/17/22</p></div>
+          <p>Generated Suggestion</p>
+        </div>
+      </div>
+      <br></br>
+
+      {/* {journals.map((journal) => {
+        return <Journal journal={journal} key={journal.id} />
+      })} */}
+    </>
+
+    
   );
 }
 
 export default App;
+
+// cursor parking lot
+// -------------------
+// |                 |
+// |                 | 
+// |                 |
+// -------------------
---
+++ client/src/Components/NavBar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+
+// function Navbar() {
+//   return(
+//     <Wrapper>
+//         <Logo>
+
+//         </Logo>
+//         <Nav>
+            
+//         </Nav>
+      
+//     </Wrapper>
+//   )
+// }
+
+
+// const Wrapper = styled.header`
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   padding: 8px;
+// `;
+
+// const Logo = styled.h1`
+//   font-family: "Permanent Marker", cursive;
+//   font-size: 3rem;
+//   color: deeppink;
+//   margin: 0;
+//   line-height: 1;
+
+//   a {
+//     color: inherit;
+//     text-decoration: none;
+//   }
+// `;
+
+// const Nav = styled.nav`
+//   display: flex;
+//   gap: 4px;
+//   position: absolute;
+//   right: 8px;
+// `;
